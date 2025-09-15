105f3a8d008723eae0fe72c2521c68730c310c33
samuel <samuel@iesjuanbosco.es>
2025-09-15 10:58:21 +0200

    Tercer commint

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hola, este es nuestra primera aplicación usando IntelliJ!!");
+        System.out.printf("Esta es una actualización del proyecto.");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
